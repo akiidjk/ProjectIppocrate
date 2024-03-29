68d86a6f7285e4e98982e03464f48fa9782abbfd
Retr0 <dragonfaiver@gmail.com>
2024-03-29 12:29:18 +0100

    Added logging

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,31 +1,36 @@
 mod data;
 
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder, web};
+use actix_web::middleware::Logger;
+use env_logger::Env;
+use log::{info, warn, error};
 use redis::{Client, RedisResult};
 use crate::data::URL_REDIS;
 
 #[get("/")]
 async fn hello() -> impl Responder {
+    info!("Handling request for /hello endpoint");
     HttpResponse::Ok().body("Server online!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("Server started");
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
 
     // Setup Redis
     let client = Client::open(URL_REDIS).expect("Failed to create Redis client");
 
     match client.get_connection() {
-        Ok(_) => println!("Connection created with Redis"),
-        Err(err) => println!("Failed to create connection with Redis: {}", err),
+        Ok(_) => info!("Connection created with Redis"),
+        Err(err) => error!("Failed to create connection with Redis: {}", err),
     }
-    
+
     HttpServer::new(|| {
         App::new()
             .service(hello)
+            .wrap(Logger::default())
     })
-        .bind(("0.0.0.0", 8080))?
+        .bind(("127.0.0.1", 8080))?
         .run()
         .await
 }
