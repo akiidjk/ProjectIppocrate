5c6c4e62ffa64691c4d48807bb28663593a733d5
Retr0 <dragonfaiver@gmail.com>
2024-03-30 13:05:57 +0100

    Add redis connection pool (globaly)

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,11 +1,22 @@
 mod data;
+mod model;
 
+use std::env;
+use std::sync::Mutex;
+use std::time::Duration;
 use actix_web::{get, App, HttpResponse, HttpServer, Responder, web};
 use actix_web::middleware::Logger;
+use actix_web::web::Data;
+use deadpool_redis::{Config, Pool, Runtime};
+use deadpool_redis::redis::cmd;
 use env_logger::Env;
 use log::{info, warn, error};
-use redis::{Client, RedisResult};
+use redis::{Client, Commands, Connection, RedisResult};
+use redis::aio::MultiplexedConnection;
+use serde_json::json;
+
 use crate::data::URL_REDIS;
+
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -13,21 +24,54 @@ async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Server online!")
 }
 
+#[get("/test")]
+async fn test(pool: Data<Pool>) -> impl Responder {
+
+    let value = json!({
+        "name": "John Doe",
+        "age": 30,
+        "is_active": false
+    });
+
+    let value_str:&str = &*serde_json::to_string(&value).unwrap();
+    let mut conn = pool.get().await.unwrap();
+
+    cmd("SET")
+            .arg(&["deadpool/test_key1", &value_str])
+            .query_async::<_, ()>(&mut conn)
+            .await.unwrap();
+
+    HttpResponse::Ok().body("test")
+}
+
+
+
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
     env_logger::init_from_env(Env::default().default_filter_or("info"));
 
     // Setup Redis
-    let client = Client::open(URL_REDIS).expect("Failed to create Redis client");
+    // let client = Client::open(URL_REDIS).expect("Failed to create Redis client");
+    //
+    //
+    // let value = json!({
+    //     "name": "John Doe",
+    //     "age": 30,
+    //     "is_active": false
+    // });
+    let mut cfg = Config::from_url(URL_REDIS);
+    let pool = cfg.create_pool(Some(Runtime::Tokio1)).unwrap();
+    let pool_data = Data::new(pool);
 
-    match client.get_connection() {
-        Ok(_) => info!("Connection created with Redis"),
-        Err(err) => error!("Failed to create connection with Redis: {}", err),
-    }
 
-    HttpServer::new(|| {
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(pool_data.clone())
             .service(hello)
+            .service(test)
             .wrap(Logger::default())
     })
         .bind(("0.0.0.0", 8080))?
