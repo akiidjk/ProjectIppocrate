c5a5abf35ad1bdd53122959a8a21837ba9784a30
Retr0 <dragonfaiver@gmail.com>
2024-04-28 14:46:07 +0200

    Implemented custom JWT for the access to bearer TOKEN

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,40 @@
 import NextAuth from "next-auth/next";
 import type { AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials"
-import bcrypt from "bcrypt";
+// import bcrypt from "bcrypt";
+
+const ENDPOINT_AUTH = "http://localhost:8000/admin/auth";
 
 export const authOptions: AuthOptions = {
     session : {
       strategy: "jwt"
     },
+    adapter: {
+
+    },
+    jwt: {
+        maxAge: 60 * 60,
+        // async encode(params: { token: JWT,secret: string,maxAge: number }): Promise<string> {
+        //     return String(params.token);
+        // },
+        // async decode(params: {token: string,secret: string}): Promise<JWT | null> {
+        //     console.log(params.token)
+        //     console.log(params.secret)
+        //     console.log(jwt_decode(params.token))
+        //     return {}
+        // },
+    },
+    callbacks: {
+        async jwt({ user, token }) {
+            //TODO Fix the issue of the parameter (In fact, if you add a parameter to user when trying to retrieve the added value and thus not a default key, it probably disappears due to a TYPE issue )
+            if (user) {
+                token.name = user.token;
+            }
+            return token;
+        },
+    },
     secret: "WMrnFPX8Gafw1BFouLJRRKgCgf/VtKutrgHoScVPobc=",
-    debug: true, //production
+    debug: true,
     providers : [
         CredentialsProvider({
             name: 'credentials',
@@ -16,20 +42,33 @@ export const authOptions: AuthOptions = {
               username: { label: "Username", type: "text", placeholder: "jsmith" },
               password: { label: "Password", type: "password" }
             },
-            async authorize(credentials, req) {
-                const res = await fetch("/your/endpoint", {
-                    method: 'POST',
-                    body: JSON.stringify(credentials),
-                    headers: { "Content-Type": "application/json" }
+            authorize: async function (credentials, req) {
+
+                if(credentials?.username === '' || credentials?.password === '') {
+                    return null
+                }
+
+                let headers = {
+                    'Authorization': `Basic ${btoa(`${credentials?.username}:${credentials?.password}`)}`
+                }
+
+                const res = await fetch(ENDPOINT_AUTH, {
+                    method: 'GET',
+                    headers: headers
                 })
-                const user = await res.json()
-            
+
+                const token = await res.json()
+
+                const user = {
+                    token: token,
+                }
+
                 if (res.ok && user) {
                     return user
                 }
                 return null
             }
-          })
+        })
     ],
 
 } 
