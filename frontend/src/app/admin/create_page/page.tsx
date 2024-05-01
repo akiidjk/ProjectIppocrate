e5765be3bbb5ea2ee264ed8f9e1ed1724a0b5606
Retr0 <dragonfaiver@gmail.com>
2024-05-01 17:02:11 +0200

    Started to implement the admin/create_page

--- frontend/src/app/admin/create_page/page.tsx
+++ frontend/src/app/admin/create_page/page.tsx
@@ -2,6 +2,15 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
+import {
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
+} from "@/components/ui/resizable"
+
+import Loader from "@/app/components/loader";
+
+
 export default function CreateAdminPage() {
     const { data: session, status } = useSession();
     const router = useRouter();
@@ -12,14 +21,37 @@ export default function CreateAdminPage() {
     }
 
     if (status === "loading") {
-        return <div>Caricamento...</div>;
+        return <Loader/>;
     }
 
     console.log("Token Bearear:", session?.user?.name);
 
     return (
-        <div>
-            <p>Pagina protetta</p>
+        <div className="h-screen">
+            <ResizablePanelGroup direction="horizontal">
+                <ResizablePanel defaultSize={30} minSize={20}>
+                    <div className="m-7 flex">
+                        <h1 className="ms-auto me-auto text-4xl font-semibold">
+                        Form
+                            </h1>
+                    </div>
+                </ResizablePanel>
+                <ResizableHandle />
+                <ResizablePanel defaultSize={70} minSize={40}>
+                    <div
+                        className="h-screen w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2]">
+                        <div className="flex">
+                            <h1 className="ms-auto me-auto text-4xl mt-7 font-semibold">
+                                Preview page
+                            </h1>
+                        </div>
+                        <div className="h-screen flex items-center justify-center">
+                            <h1 className="font-medium text-3xl">Content Preview</h1>
+                        </div>
+                    </div>
+                </ResizablePanel>
+            </ResizablePanelGroup>
         </div>
-    );
+    )
+        ;
 }
