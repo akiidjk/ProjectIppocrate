04e87a719ea4a9e824fb22c0ce2d222e3c05ec16
Retr0 <dragonfaiver@gmail.com>
2024-05-02 15:23:16 +0200

    Fixed the button_see_more.tsx

--- frontend/src/app/components/button_see_more.tsx
+++ frontend/src/app/components/button_see_more.tsx
@@ -3,10 +3,12 @@ import Link from "next/link";
 export default function ButtonSeeMore(){
     return(
     
-        <div className="flex m-8">
-           <button className="animated-button ms-auto me-auto">
-                <Link href="/subject_list">More</Link>
-            </button>
+        <div className="flex m-8 ">
+            <Link href="/subject_list" className="ms-auto me-auto">
+               <button className="animated-button ">
+                    More
+                </button>
+            </Link>
         </div>
     )
 }
