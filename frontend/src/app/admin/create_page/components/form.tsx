c47dc5f0d9a7671bfd987b9ed265a92574805a0d
Retr0 <dragonfaiver@gmail.com>
2024-05-02 15:11:49 +0200

    Continue to creating the page for CMS

---
+++ frontend/src/app/admin/create_page/components/form.tsx
@@ -0,0 +1,129 @@
+import { Input } from "@/components/ui/input"
+import {Label} from "@/components/ui/label";
+import QuestionMark from "@/app/admin/create_page/components/svg/question_mark";
+import { Progress } from "@/components/ui/progress"
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip"
+import {Button} from "@/components/ui/button";
+import Plus from "@/app/admin/create_page/components/svg/plus";
+import { Separator } from "@/components/ui/separator"
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
+import { useToast } from "@/components/ui/use-toast"
+
+export default  function Form(){
+    const { toast } = useToast()
+
+    return (
+        <div>
+            <div className="w-full flex flex-col">
+                <div className="mx-10">
+                    <div className="flex">
+                        <Label htmlFor="page_title" className="text-[18px]">Titolo pagina</Label>
+                        <TooltipProvider>
+                            <Tooltip>
+                                <TooltipTrigger>
+                                    <QuestionMark color="#4c69c3" size="20px" className="ml-3"/>
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                    <p>Imposta il titolo della pagina</p>
+                                </TooltipContent>
+                            </Tooltip>
+                        </TooltipProvider>
+                    </div>
+                    <Input type="text" id="title" placeholder="Storia" className="mt-3"/>
+                </div>
+            </div>
+
+            <div className="w-full flex flex-col mt-6">
+                <div className="me-auto ms-auto">
+                    <div className="flex">
+                        <Label htmlFor="page_title" className="text-[18px]">Inserisci paragrafi</Label>
+                        <TooltipProvider>
+                            <Tooltip>
+                                <TooltipTrigger>
+                                    <QuestionMark color="#4c69c3" size="20px" className="ml-3"/>
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                    <p>Crea i paragrafi da inserire nella pagina</p>
+                                </TooltipContent>
+                            </Tooltip>
+                        </TooltipProvider>
+                    </div>
+                    <div className="flex">
+                        <Button onClick={() => console.log("ADD")} variant="ghost" size="icon" className="ms-auto mt-3 me-auto bg-[#fdfdfd]">
+                            <Plus color={"#18181b"} size={"30px"}/>
+                        </Button>
+                    </div>
+                </div>
+            </div>
+            <Separator className="my-4 w-3/4 me-auto ms-auto"/>
+            <div className="w-2/3 me-auto ms-auto mt-6">
+                <Progress value={33}/>
+            </div>
+
+            <div className="flex">
+                <div className="ms-auto">
+                    <AlertDialog>
+                        <AlertDialogTrigger asChild>
+                            <Button variant="destructive" className="mt-5 mr-5">Ripristina</Button>
+                        </AlertDialogTrigger>
+                        <AlertDialogContent>
+                            <AlertDialogHeader>
+                                <AlertDialogTitle>Sei assolutamente sicuro?</AlertDialogTitle>
+                                <AlertDialogDescription>
+                                    Questa azione non può essere annullata. L'operazione eliminerà definitivamente il vostro
+                                    progresso di creazione.
+                                </AlertDialogDescription>
+                            </AlertDialogHeader>
+                            <AlertDialogFooter>
+                                <AlertDialogCancel>No meglio di no</AlertDialogCancel>
+                                <AlertDialogAction className="bg-[#f05656] hover:bg-[#ff665b]">Si ripristina!</AlertDialogAction>
+                            </AlertDialogFooter>
+                        </AlertDialogContent>
+                    </AlertDialog>
+
+                    <AlertDialog>
+                        <AlertDialogTrigger asChild>
+                            <Button className="mt-5 mr-5">Salva</Button>
+                        </AlertDialogTrigger>
+                        <AlertDialogContent>
+                            <AlertDialogHeader>
+                                <AlertDialogTitle>Sei assolutamente sicuro?</AlertDialogTitle>
+                                <AlertDialogDescription>
+                                    Confermando metti fine al processo di creazione non potrai piu modificare la pagina ma solo eliminarla succesivamente
+                                </AlertDialogDescription>
+                            </AlertDialogHeader>
+                            <AlertDialogFooter>
+                                <AlertDialogCancel>No meglio di no</AlertDialogCancel>
+                                <AlertDialogAction onClick={() => {  {/*Implement the logic*/}
+                                    toast({
+                                                    className:"bg-[#3aba6f] text-[#fdfdfd]",
+                                                    description: "Pagina salvata e inviata con successo",
+                                                })
+                                            }}>
+                                            Si salva!
+                                    </AlertDialogAction>
+                            </AlertDialogFooter>
+                        </AlertDialogContent>
+                    </AlertDialog>
+                </div>
+            </div>
+
+        </div>
+    )
+}
--- frontend/src/app/admin/create_page/page.tsx
+++ frontend/src/app/admin/create_page/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import {useEffect, useRef, useState} from "react";
 
 import {
     ResizableHandle,
@@ -9,11 +10,27 @@ import {
 } from "@/components/ui/resizable"
 
 import Loader from "@/app/components/loader";
+import Form from "@/app/admin/create_page/components/form";
+import Navbar from "@/app/components/navbar";
+import {Button} from "@/components/ui/button";
+import Expand from "@/app/admin/create_page/components/svg/expand";
+
+import {ImperativePanelHandle} from "react-resizable-panels";
+
+
 
 
 export default function CreateAdminPage() {
     const { data: session, status } = useSession();
     const router = useRouter();
+    const [isFull, setIsFull] = useState(false);
+
+    const resizeNull = useRef<ImperativePanelHandle>(null);
+
+    useEffect(() => {
+        resizeNull.current?.resize(0)
+    }, [isFull]);
+
 
     if (status === "unauthenticated") {
         router.replace("/admin/login");
@@ -27,31 +44,46 @@ export default function CreateAdminPage() {
     console.log("Token Bearear:", session?.user?.name);
 
     return (
-        <div className="h-screen">
-            <ResizablePanelGroup direction="horizontal">
-                <ResizablePanel defaultSize={30} minSize={20}>
-                    <div className="m-7 flex">
-                        <h1 className="ms-auto me-auto text-4xl font-semibold">
-                        Form
-                            </h1>
-                    </div>
-                </ResizablePanel>
-                <ResizableHandle />
-                <ResizablePanel defaultSize={70} minSize={40}>
-                    <div
-                        className="h-screen w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2]">
-                        <div className="flex">
-                            <h1 className="ms-auto me-auto text-4xl mt-7 font-semibold">
-                                Preview page
-                            </h1>
+        <div>
+            <Navbar/>
+            <div>
+                <ResizablePanelGroup direction="horizontal">
+                    {
+                        isFull ?
+                            <ResizablePanel defaultSize={0} minSize={0} ref={resizeNull}></ResizablePanel>
+                            :
+                            <ResizablePanel defaultSize={30} minSize={20}>
+                                <div className="m-7 flex ">
+                                    <h1 className="ms-auto me-auto text-4xl font-semibold">
+                                        Form
+                                    </h1>
+                                </div>
+                                <div>
+                                    <Form/>
+                                </div>
+                            </ResizablePanel>
+                    }
+                    <ResizableHandle/>
+                    <ResizablePanel defaultSize={70} minSize={65}>
+                        <div
+                            className="w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2]">
+                            <div className="relative top-2 left-2">
+                                <Button onClick={() => setIsFull(!isFull)} size="icon">
+                                    <Expand color={"#ffffff"} size={"30px"}/>
+                                </Button>
+                            </div>
+                            <div className="flex">
+                                <h1 className="ms-auto me-auto text-4xl mt-7 font-semibold">
+                                    Preview page
+                                </h1>
+                            </div>
+                            <div className="h-[81vh] flex items-center justify-center">
+                                <h1 className="font-medium text-3xl">Content Preview</h1>
+                            </div>
                         </div>
-                        <div className="h-screen flex items-center justify-center">
-                            <h1 className="font-medium text-3xl">Content Preview</h1>
-                        </div>
-                    </div>
-                </ResizablePanel>
-            </ResizablePanelGroup>
+                    </ResizablePanel>
+                </ResizablePanelGroup>
+            </div>
         </div>
-    )
-        ;
+    );
 }
