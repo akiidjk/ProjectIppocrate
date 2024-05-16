f8efd2c28b040b4f8d5f2fac99de6bbb5f5cc3bc
Retr0 <dragonfaiver@gmail.com>
2024-05-16 20:34:40 +0200

    Fixed port

--- frontend/src/app/api/api.ts
+++ frontend/src/app/api/api.ts
@@ -1,7 +1,8 @@
 import {Page} from "@/context/page_provider";
 
 // const BASE_ENDPOINT = "http://localhost:8000";
-const BASE_ENDPOINT = "http://backend:8000";
+// const BASE_ENDPOINT = "http://backend:8000
+const BASE_ENDPOINT = "http://backend";
 
 export type FileStringTuple = [File, string];
 
